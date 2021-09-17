6c87210660821255beffe5333353f8e6c94085f6
Manmeet Singh <manmeet.singh2021@vitstudent.ac.in>
2021-09-17 12:01:39 +0530

    Fourth commit

--- djproject/urls.py
+++ djproject/urls.py
@@ -3,6 +3,6 @@ from djproject import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
